63bbe1e4e622c03df5020e3533dbd9345676e37a
Pedro Revilla <peterevilla@pedros-air.attlocal.net>
2020-04-26 23:03:22 -0500

    mvp

--- friends/src/components/AddFriend.js
+++ friends/src/components/AddFriend.js
@@ -64,7 +64,7 @@ function AddFriend() {
               <div className="flex items-center justify-between">
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                  type="submit">
-                  Sign In
+                  Add Friend
                 </button>
               </div>
             </form>
